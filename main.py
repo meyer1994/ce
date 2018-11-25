34825ffbc8f13cf63aad859cdee5fad9c88f0add
Joao Meyer <1994meyer@gmail.com>
2018-11-25 12:27:24 -0200

    Now it also generates after validating

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from argparse import ArgumentParser
 
+from llvmlite import ir
+
+from ce.scope import Scopes
 from ce.parser import create_parser
+
+
+def create_builder():
+    '''
+    Creates a simple builder for testing purposes
+
+    The builder has a void function and an empty basic block in it.
+    '''
+    module = ir.Module(name='test_module')
+    typ = ir.FunctionType(ir.VoidType(), ())
+    function = ir.Function(module, typ, name='test_function')
+    block = function.append_basic_block(name='test_block')
+    return ir.IRBuilder(block)
+
 
 if __name__ == '__main__':
     arg_parser = ArgumentParser(description='Parse a file')
@@ -10,8 +27,14 @@ if __name__ == '__main__':
     with open(args.file, 'r') as f:
         data = f.read()
         parser = create_parser()
-        parser.parse(data)
+        result = parser.parse(data)
 
-    # builder = result.generate(None, scope)
+    scope = Scopes()
+    result.validate(scope)
+    for r in result.commands:
+        print(r)
 
-    # print(builder.module)
+    scope = Scopes()
+    builder = create_builder()
+    result.generate(builder, scope)
+    print(builder.module)

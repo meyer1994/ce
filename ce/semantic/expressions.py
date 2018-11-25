2674c0674be2c066dc93c8456af3bbb9e58209bf
Joao Meyer <1994meyer@gmail.com>
2018-11-25 12:04:41 -0200

    Separared more

---
+++ ce/semantic/expressions.py
@@ -0,0 +1,41 @@
+from ce.semantic.node import Node
+from ce.types import OperationTypes, NumericTypes, Types, cast
+
+
+class OpBin(Node):
+    def __init__(self, left, operation, right):
+        super(OpBin, self).__init__()
+        self.left = left
+        self.operation = operation
+        self.right = right
+
+    def validate(self, scope):
+        self.left.validate(scope)
+        self.right.validate(scope)
+
+        if self.operation == OperationTypes.BOOLEAN:
+            self._boolean()
+            self._type = Types.BOOLEAN
+        else:
+            self._type = cast(self.left.type, self.right.type)
+
+    def _boolean(self):
+        left = self.left.type
+        right = self.right.type
+        if not (left in NumericTypes and right in NumericTypes):
+            error = '(%s, %s)' % (left, right)
+            raise Exception('Both sides must be numeric. Got %s' % error)
+
+
+class OpUn(Node):
+    def __init__(self, operation, right):
+        super(OpUn, self).__init__()
+        self.operation = operation
+        self.right = right
+
+    def validate(self, scope):
+        self.right.validate(scope)
+        if self.right.type not in NumericTypes:
+            raise Exception('Unary operation must be with numbers')
+        self._type = self.right.type
+
